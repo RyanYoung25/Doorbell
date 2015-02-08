105c6b28871ecc5ed4ca68813403b8c35d3bf33d
Ryan Young <Ryan@rdyoung.us>
2015-02-08 17:42:09 -0500

    Got the server working, now to work on the lights

---
+++ blinkLights.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import Rpi.GPIO as GPIO
+import time
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    #Default 7, 11, 13
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+
+def blink():
+    setup()
+    while True:
+        GPIO.output(7, True)
+        time.sleep(.5)
+        GPIO.output(7, False)
+        time.sleep(.5)
--- server.py
+++ server.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
-import socket
+import BaseHTTPServer
+from blinkLights import blink
+
+
+class DoorBellHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Ryan's Doorbell</title></head><body>")
+        s.wfile.write("<p>Press this button to alert Ryan that his presence is requested in the living room.</p><button id='but1'>This is the button</button>")
+        #Adding jquery
+        s.wfile.write("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>")
+        #Adding java scirpt for button press
+        #Uses the static IP 192.168.0.101 because my RaspPi has a DCHP reservation for that address on our router
+        s.wfile.write("<script type='text/javascript'>$('#but1').click(function(){ $.ajax({url:'http://192.168.0.101:8000/server.py', type:'POST'}); alert('Ryan Has been notified');}); </script>")
+        s.wfile.write("</body></html>")
+
+    def do_POST(s):
+        blink()
+
+
+def startServer():
+    server_address = ('', 8000)
+    server = BaseHTTPServer.HTTPServer(server_address, DoorBellHandler)
+    server.serve_forever()
+
 
 def main():
-
-    ipAddr = "192.168.0.101"
-    portNum = 80
-    bufferSize = 2048
-    message = "Can I give you an html file?"
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((ipAddr, portNum))
-    s.listen(0)
-
-    sc, address = s.accept()
-    print address
-    sc.send(message)
-    sc.close()
-    s.close()
+    startServer()
 
 if __name__ == "__main__":
     main()
