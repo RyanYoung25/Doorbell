d4203f4394e8f296b5a34bfe200268a73cba2f53
Leo is awesome <lc599@drexel.edu>
2015-02-08 21:38:04 -0500

    can now post message to ryan

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import BaseHTTPServer
+import BaseHTTPServer, cgi
+from time import gmtime, strftime
 from blinkLights import *
 
 
@@ -14,17 +15,46 @@ class DoorBellHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s.wfile.write("<html><head><title>Ryan's Doorbell</title></head><body>")
-        s.wfile.write("<p>Press this button to alert Ryan that his presence is requested in the living room.</p><button id='but1'>This is the button</button>")
-        #Adding jquery
-        s.wfile.write("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>")
-        #Adding java scirpt for button press
+
         #Uses the static IP 192.168.0.101 because my RaspPi has a DCHP reservation for that address on our router
-        s.wfile.write("<script type='text/javascript'>$('#but1').click(function(){ $.ajax({url:'http://192.168.0.101:8000/server.py', type:'POST'}); alert('Ryan Has been notified');}); </script>")
-        s.wfile.write("</body></html>")
+        html = """
+            <html>
+                <head>
+                    <title>Ryan's Doorbell</title>
+                </head>
+                <body>
+                    <p>Press this button to alert Ryan that his presence is requested in the living room.</p>
+                    <button id='but1'>This is the button</button><br><br>
+                    <textarea id="message" style="min-width: 400px; min-height: 100px;" placeholder="Leave blank or enter a message for Ryan here ..."></textarea>
+
+                    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+                    <script type='text/javascript'>
+                        $('#but1').click(function(){
+                            $.ajax({
+                                url:'http://192.168.0.101:8000/server.py',
+                                type:'POST',
+                                data: {
+                                    message: $("#message").val().trim() || "Get thineself into the living area quarters"
+                                }
+                            });
+                            alert('Ryan has been notified');
+                        });
+                    </script>
+                </body>
+            </html>
+        """
+        s.wfile.write(html)
 
     def do_POST(s):
         blink()
+
+        # Retrieve the POST parameters
+        ctype, pdict = cgi.parse_header(s.headers.getheader('content-type'))
+        length = int(s.headers.getheader('content-length'))
+        postVars = cgi.parse_qs(s.rfile.read(length), keep_blank_values=1)
+
+        message = postVars["message"][0]
+        print "[" + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + "] " + message
 
 
 def startServer():
